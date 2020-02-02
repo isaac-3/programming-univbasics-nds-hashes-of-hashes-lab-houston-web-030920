a1a899ca9b3a959ad5cd33390571d8d16e7d8dca
isaac chavez <isaacgc0596@gmail.com>
2020-02-02 00:34:24 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,7 +41,11 @@ def naming_system
         sub_category: {
           label: "Family",
           sub_category: {
-            label: "Genus"
+            label: "Genus",
+            sub_category: {
+              label: "Species",
+              sub_category: nil
+            }
           }
         }
       }
