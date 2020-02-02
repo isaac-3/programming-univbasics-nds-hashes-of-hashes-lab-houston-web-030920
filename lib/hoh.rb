177e8d535ae375a083ccf07ee49f4d772028baf5
isaac chavez <isaacgc0596@gmail.com>
2020-02-02 00:32:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,7 +38,7 @@ def naming_system
       label: "Class",
       sub_category: {
         label: "Order",
-        sub_category : {
+        sub_category: {
           label: "Family",
           sub_category: {
             label: "Genus"
